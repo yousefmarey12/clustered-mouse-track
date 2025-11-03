386c0418d805b0a67353d3b356f106e17d5a4648
yousefmarey12 <yousef.marey@ucalgary.ca>
2025-11-03 21:15:09 +0300

    feat(index.js): initial commit
    used Node.js Cluster Module, Socket.io, and Express to draw graphs of mouse activity on a client
    
    BREAKING CHANGE: first commit in production

---
+++ index.js
@@ -0,0 +1,114 @@
+let express = require('express')
+let cors = require('cors')
+let path = require('path')
+let fs = require('fs')
+let uuid = require('uuid')
+let cluster = require('node:cluster')
+let os = require('os')
+let app = express(cors())
+let http = require('http')
+const { setupMaster, setupWorker } = require("@socket.io/sticky");
+const { Server } = require('socket.io');
+const { setupPrimary, createAdapter } = require('@socket.io/cluster-adapter')
+
+
+let numCpus = os.cpus()
+
+if (cluster.isPrimary) {
+    const server = http.createServer();
+    server.listen(5000);
+    setupMaster(server, {
+        loadBalancingMethod: 'least-connection'
+    })
+
+    setupPrimary({
+        serialization: "advanced"
+    })
+
+
+    for (let i = 0; i < numCpus.length; i++) {
+        let worker = cluster.fork()
+
+    }
+
+    cluster.on('exit', (worker) => {
+        console.log(`Worker with ${worker.process.pid} died`)
+        cluster.fork()
+    })
+}
+
+if (cluster.isWorker) {
+    // works across all workers using @socket.io/cluster-adapter
+
+    console.log(`Worker with Process id is running: ${process.pid}`)
+    const httpServer = http.createServer((req, res) => {
+        if (req.url === '/admin') {
+            fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res)
+        } else {
+            fs.createReadStream(path.join(__dirname, 'client.html')).pipe(res)
+        }
+    })
+    const io = new Server(httpServer);
+    io.adapter(createAdapter());
+    setupWorker(io);
+
+
+    let addGraph = (socket, obj) => {
+        socket.emit('addUser', obj)
+    }
+    let removeGraph = (socket, id) => {
+        console.log("Here is it removed")
+        socket.emit('removeUser', id)
+    }
+    let updateGraph = (socket, obj) => {
+        socket.emit('updateUser', obj)
+    }
+
+    io.of('/admin').on('removeUser', obj => {
+
+    })
+    io.of('/admin').on('addUser', () => { })
+    io.of('/admin').on('connection', socket => {
+
+    })
+
+    io.of('/client').on('connection', socket => {
+        let obj = Object.create(null)
+        obj.line = []
+        obj.count = 0
+        obj.date = Date.now()
+        obj.id = uuid.v4()
+        io.of('/admin').emit('addUser', obj)
+
+        let interval = setInterval(() => {
+
+            io.of('/admin').emit('updateUser', obj)
+            obj.count = 0
+            obj.date = Date.now()
+
+        }, 1000)
+
+
+
+        socket.on('mouse_move', () => {
+
+            obj.count++
+        })
+
+        socket.on('line', connection => {
+            let line = connection.obj
+            obj.line = line
+        })
+
+        socket.on('disconnect', () => {
+            io.of('/admin').emit('removeUser', obj.id)
+            interval.close()
+        })
+
+
+    })
+
+
+
+}
+
